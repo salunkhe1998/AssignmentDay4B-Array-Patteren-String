d7378e9d36a4f9c1fec00a87870db4d83020426a
Ashwini <salunkheashwini1998@gmail.com>
2023-03-23 15:19:47 +0530

    To Duplicate Array Element

---
+++ DuplicateElementinArray.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PatternAssigment
+{
+    public class DuplicateElementinArray
+    {
+        public static void CheckDuplicate()
+        {
+            int[] arr = new int[] { 1, 2, 3, 4, 2, 7, 8, 8, 3 };
+
+            Console.WriteLine("Duplicate elements in given array: ");
+            for (int i = 0; i < arr.Length; i++)
+            {
+                for (int j = i + 1; j < arr.Length; j++)
+                {
+                    if (arr[i] == arr[j])
+                        Console.WriteLine(arr[j]);
+                }
+            }
+        }
+    }
+}
+    
+
